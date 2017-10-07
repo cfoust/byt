016dcb4653b16ab1a1f8c037ee4b97e1b38e4339
Caleb Foust <cfoust@sqweebloid.com>
2017-10-07 16:04:12 -0500

    feat(PieceFile): add tests for `insert`

--- src/byt/io/file.rs
+++ src/byt/io/file.rs
@@ -6,8 +6,10 @@
 // EXTERNS
 
 // LIBRARY INCLUDES
+use std::fmt;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::io::Empty;
 use std::io::BufReader;
 use std::io::Read;
 use std::io;
@@ -16,7 +18,7 @@ use std::io;
 
 // LOCAL INCLUDES
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum SourceFile {
     /// The original file on disk.
     Original,
@@ -35,6 +37,13 @@ struct Piece {
     length : u32
 }
 
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({:?}, {}, {})", self.file, self.offset, self.length)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Operation {
     Insert,
     Delete
@@ -43,18 +52,19 @@ enum Operation {
 /// Records a particular modification of the text.
 struct Action {
     op     : Operation,
-    start  : u32,
+    offset : u32,
     length : u32
 }
 
 /// Implements logical operations on a file that are not written
 /// until asked to.
 pub struct PieceFile {
+    /// Stores all mutation operations of the file so that we can
+    /// undo.
+    actions : Vec<Action>,
     /// The in-memory append string. All edits will refer to bytes
     /// stored here.
     append_file : String,
-    /// The underlying file descriptor on the filesystem.
-    //file : File,
     /// The total size (in bytes) of the PieceFile.
     length : u32,
     /// The path of the file this PieceFile refers to.
@@ -62,7 +72,7 @@ pub struct PieceFile {
     /// Stores all current Pieces.
     piece_table : Vec<Piece>,
     /// The seekable file reader.
-    reader : BufReader<File>,
+    reader : Option<BufReader<File>>,
 }
 
 impl PieceFile {
@@ -99,11 +109,12 @@ impl PieceFile {
         let size = file.metadata().unwrap().len() as u32;
 
         let mut piece_file = PieceFile {
+            actions     : Vec::new(),
             append_file : String::new(),
-            length : size,
+            length      : size,
             path,
             piece_table : Vec::new(),
-            reader : BufReader::new(file),
+            reader      : Some(BufReader::new(file)),
         };
 
         piece_file.piece_table.push(Piece {
@@ -111,6 +122,20 @@ impl PieceFile {
             offset : 0,
             length : size as u32,
         });
+
+        Ok(piece_file)
+    }
+
+    /// Create a new empty PieceFile.
+    pub fn empty() -> io::Result<PieceFile> {
+        let mut piece_file = PieceFile {
+            actions     : Vec::new(),
+            append_file : String::new(),
+            length      : 0,
+            path        : String::from(""),
+            piece_table : Vec::new(),
+            reader      : None,
+        };
 
         Ok(piece_file)
     }
@@ -124,10 +149,18 @@ impl PieceFile {
         self.length      += length;
 
         let piece = Piece {
-            file : SourceFile::Append,
+            file   : SourceFile::Append,
+            offset : append_offset,
+            length,
+        };
+
+        let action = Action {
+            op : Operation::Insert,
             offset,
             length,
         };
+
+        self.actions.push(action);
 
         // There are edge cases if you do an insert
         // at the beginning or end of the file.
@@ -160,11 +193,7 @@ impl PieceFile {
             });
         }
 
-        self.piece_table.insert(split_index, Piece {
-            file   : SourceFile::Append,
-            offset : append_offset,
-            length,
-        });
+        self.piece_table.insert(split_index, piece);
 
         if lower_size > 0 {
             self.piece_table.insert(split_index, Piece {
@@ -173,5 +202,34 @@ impl PieceFile {
                 length : lower_size,
             });
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_inserts() {
+        let mut file = PieceFile::empty().unwrap();
+        assert_eq!(file.piece_table.len(), 0);
+
+        file.insert("foo", 0);
+        file.insert("bar", 0);
+
+        let piece_table = &file.piece_table;
+        assert_eq!(piece_table.len(), 2);
+
+        let first_element  = &piece_table[0];
+        let second_element = &piece_table[1];
+
+        assert_eq!(first_element.offset, 3);
+        assert_eq!(first_element.length, 3);
+
+        assert_eq!(second_element.offset, 0);
+        assert_eq!(second_element.length, 3);
+
+        let action = &file.actions[0];
+        assert_eq!(action.op, Operation::Insert);
     }
 }
