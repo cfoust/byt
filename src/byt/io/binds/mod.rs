b78e6bcda33d662811fc7b98faadc207297e6861
Caleb Foust <cfoust@sqweebloid.com>
2017-12-17 07:54:44 -0600

    feat(binds): add wildcard field

--- src/byt/io/binds/mod.rs
+++ src/byt/io/binds/mod.rs
@@ -14,19 +14,31 @@ use termion::event::Key;
 /// Stores a reference to the action identifier or the
 /// next binding table.
 pub enum Next<'b> {
+    /// The string referring to a method e.g `next-line`.
     Action(String),
-    Table(&'b BindingTable<'b>)
+    /// A reference to another table.
+    Table(&'b BindingTable<'b>),
+    /// Go back to the root table.
+    Root,
+    /// Stay in the current table and do nothing.
+    Noop
 }
 
 /// The association of a key to some action.
 pub struct Binding<'a> {
-    pub key    : Key,
-    pub result : Next<'a>
+    // The key that will yield the result.
+    key    : Key,
+    // Either an action or a table of new bindings.
+    result : Next<'a>,
 }
 
 /// A table of bindings.
 pub struct BindingTable<'a> {
-    pub bindings : Vec<Binding<'a>>
+    bindings : Vec<Binding<'a>>,
+    /// Describes what happens when a key matches nothing in the list 
+    /// of bindings.  If `wildcard` is an Action, it is invoked with
+    /// the key.
+    wildcard : Next<'a>
 }
 
 impl<'a> BindingTable<'a> {
@@ -43,6 +55,8 @@ impl<'a> BindingTable<'a> {
 
     /// Add a binding that runs an action.
     pub fn add_action(&mut self, key : Key, action : String) {
+        // Note: in the future it might be good to ensure that only
+        // one binding exists for a given key in the table.
         self.bindings.push(Binding {
             key    : key.clone(),
             result : Next::Action(action)
@@ -56,4 +70,54 @@ impl<'a> BindingTable<'a> {
             result : Next::Table(table)
         });
     }
+}
+
+/// Takes in keys and returns actions or tables.
+pub struct BindHandler<'b> {
+    current_table : &'b BindingTable<'b>,
+    root_table    : &'b BindingTable<'b>,
+    next_action   : &'b str,
+    has_action    : bool,
+}
+
+impl<'a> BindHandler<'a> {
+    // ###############################
+    // P U B L I C  F U N C T I O N S
+    // ###############################
+
+    pub fn new(table : &'a BindingTable<'a>) -> BindHandler<'a> {
+        BindHandler {
+            current_table : table,
+            root_table    : table,
+            next_action   : "",
+            has_action    : false
+        }
+    }
+
+    /// Handle a key of new user input.
+    pub fn consume(&mut self, key : Key) {
+        for binding in self.current_table.bindings.iter() {
+            if key != binding.key {
+                continue;
+            }
+
+            match binding.result {
+                Next::Action(ref action) => {
+                    self.next_action = action.as_str();
+                    self.has_action  = true;
+                    return;
+                },
+                Next::Table(table) => {
+                    self.current_table = table;
+                    return;
+                },
+                Next::Root => {
+                    self.current_table = self.root_table;
+                    return;
+                }
+            }
+        }
+    }
+
+    /// Check whether an action can be consumed.
 }
