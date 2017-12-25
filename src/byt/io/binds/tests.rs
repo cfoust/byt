1c6e6da6f4406d71aa2d9ac2d2b788b93a95c036
Caleb Foust <cfoust@sqweebloid.com>
2017-12-24 21:06:02 -0600

    feat(binds): add pop test

--- src/byt/io/binds/tests.rs
+++ src/byt/io/binds/tests.rs
@@ -50,18 +50,6 @@ fn it_finds_a_binding() {
 }
 
 #[test]
-fn it_finds_a_wildcard() {
-    let mut master = Keymaster::new();
-
-    let mut table = BindingTable::new();
-    table.set_wildcard(Action::Function(String::from("foobar")));
-
-    master.add_table(table);
-
-    assert!(master.search_key(Key::Char('a')).is_some());
-}
-
-#[test]
 fn it_finds_a_lower_binding() {
     let mut master = Keymaster::new();
     let mut global = BindingTable::new();
@@ -78,4 +66,38 @@ fn it_finds_a_lower_binding() {
     master.add_table(BindingTable::new());
 
     assert!(master.search_key(Key::Char('a')).is_some());
+}
+
+#[test]
+fn it_finds_a_wildcard() {
+    let mut master = Keymaster::new();
+
+    let mut table = BindingTable::new();
+    table.set_wildcard(Action::Function(String::from("foobar")));
+
+    master.add_table(table);
+
+    assert!(master.search_key(Key::Char('a')).is_some());
+}
+
+#[test]
+fn it_pops_a_table() {
+    let mut master = Keymaster::new();
+
+    let mut first = BindingTable::new();
+    first.add_binding(Binding {
+        key : Key::Char('a'),
+        result : Action::Nothing
+    });
+    master.add_table(first);
+
+    let mut second = BindingTable::new();
+    second.add_binding(Binding {
+        key : Key::Char('a'),
+        result : Action::Pop
+    });
+    master.add_table(second);
+
+    master.consume(Key::Char('a'));
+    assert!(master.tables.len() == 1);
 }
