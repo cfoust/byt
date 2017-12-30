9d5d6c4ed9a5ea1094c4cfc72733c349b3f58fc9
Caleb Foust <cfoust@sqweebloid.com>
2017-12-29 20:06:27 -0600

    feat(Keymaster): try to make make_tables

--- src/byt/io/binds/mod.rs
+++ src/byt/io/binds/mod.rs
@@ -5,6 +5,11 @@
 
 // LIBRARY INCLUDES
 use termion::event::Key;
+use std::io::{
+    Error,
+    ErrorKind
+};
+use std::io;
 
 // SUBMODULES
 //mod tests;
@@ -16,7 +21,7 @@ use termion::event::Key;
 /// because it refers to the next state, even if that involves
 /// sending an action up to the editor as a result.
 #[derive(Clone)]
-pub enum Arrow {
+enum Arrow {
     /// Triggers some kind of action within the editor.
     /// In the future this will be a reference to a closure, probably.
     /// For now we just use strings for ease of testing.
@@ -50,7 +55,7 @@ impl Binding {
 }
 
 /// A table of bindings.
-pub struct BindingTable {
+struct BindingTable {
     bindings : Vec<Binding>,
     /// Describes what happens when a key matches nothing in the list
     /// of bindings. If `wildcard` is an Arrow, it is invoked with
@@ -65,31 +70,41 @@ impl BindingTable {
     // #################################
     // P R I V A T E  F U N C T I O N S
     // #################################
+
     /// Make sure that a binding does not conflict with other bindings
     /// in the table.
-    fn ensure_unique(&self, key : Key) {
+    fn ensure_unique(&self, key : Key) -> io::Result<()> {
         for binding in self.bindings.iter() {
             if key != binding.key {
                 continue;
             }
 
-            panic!("Binding already exists for key");
+            return Err(Error::new(ErrorKind::InvalidInput, "Not unique"));
         }
+
+        Ok(())
+    }
+
+    /// Get this BindingTable's unique id.
+    fn get_id(&self) -> usize {
+        self.id
     }
 
     // ###############################
     // P U B L I C  F U N C T I O N S
     // ###############################
 
-    /// Add a binding to the table.
-    pub fn add_binding(&mut self, binding : Binding) {
-        self.ensure_unique(binding.key);
-        self.bindings.push(binding);
-    }
+    /// Bind some an action to a key.
+    pub fn bind(&mut self, key : Key, action : &Arrow)
+        -> io::Result<()> {
+        self.ensure_unique(key)?;
 
-    /// Get this BindingTable's unique id.
-    pub fn get_id(&self) -> usize {
-        self.id
+        self.bindings.push(Binding {
+            key,
+            result : action.clone()
+        });
+
+        Ok(())
     }
 
     /// Make a new BindingTable without anything in it.
@@ -151,17 +166,18 @@ impl Keymaster {
     /// Get the current state (i.e the current binding table) of
     /// the Keymaster. If the current table has not been set, it
     /// will be set to the root table.
-    fn get_state(&self) -> &BindingTable {
-        self.get_table(self.current_table).unwrap()
+    fn get_state(&mut self) -> &mut BindingTable {
+        let id = self.current_table;
+        self.get_table_by_id(id).unwrap()
     }
 
     /// Get a reference to a table given its id.
-    fn get_table(&self, id : usize) -> Option<&BindingTable> {
+    fn get_table_by_id(&mut self, id : usize) -> Option<&mut BindingTable> {
         if id == 0 {
-            return Some(&self.root_table)
+            return Some(&mut self.root_table)
         }
 
-        self.tables.iter().find(|ref x| x.id == id)
+        self.tables.iter_mut().find(|ref x| x.id == id)
     }
 
     /// Interpret the result of a Arrow enum. If a function
@@ -175,12 +191,12 @@ impl Keymaster {
             Arrow::Table(ref table) => {
                 let id = *table;
 
-                if let Some(_) = self.get_table(id) {
+                if let Some(_) = self.get_table_by_id(id) {
                     self.current_table = id;
                 }
             },
             Arrow::Root => {
-                self.go_home();
+                self.to_root();
             },
             Arrow::Nothing => {}
         }
@@ -189,15 +205,16 @@ impl Keymaster {
     }
 
     /// Make a new table with an assigned id.
-    fn new_table(&mut self) -> &BindingTable {
-        self.tables.push(BindingTable::new(self.id_counter));
+    fn new_table(&mut self) -> &mut BindingTable {
+        let id = self.id_counter;
+        self.tables.push(BindingTable::new(id));
         self.id_counter += 1;
-        self.get_table(self.id_counter - 1).unwrap()
+        self.get_table_by_id(id - 1).unwrap()
     }
 
     /// Attempt to get an action for a key if it is
     /// evaluated in the current binding table.
-    fn search_key(&self, key : Key) -> Option<&Arrow> {
+    fn search_key(&mut self, key : Key) -> Option<&Arrow> {
         self.get_state().search_key(key)
     }
 
@@ -227,8 +244,108 @@ impl Keymaster {
         return self.handle_action(&action);
     }
 
+    /// Get the root binding table.
+    pub fn get_root(&mut self) -> &mut BindingTable {
+        self.get_table_by_id(0).unwrap()
+    }
+
+    /// Get a binding table according to a sequence of keys, which
+    /// are evaluated starting at the root. Will only return Some
+    /// if the keys evaluate to a state that is a table.
+    pub fn get_table<T: AsRef<[Key]>>(&mut self, sequence : T) -> Option<&mut BindingTable> {
+        let mut id = 0;
+
+        for key in sequence.as_ref().iter() {
+            let mut table = self.get_table_by_id(id).unwrap();
+            let binding = table.search_key(*key);
+
+            if binding.is_none() {
+                return None;
+            }
+
+            match *binding.unwrap() {
+                Arrow::Table(ref index) => {
+                    id = *index;
+                },
+                _ => {
+                    return None;
+                }
+            }
+        }
+
+        if id == 0 {
+            return None;
+        }
+
+        self.get_table_by_id(id)
+    }
+
+    /// Make a new table at the end of a prefix of keys. If the arrows
+    /// to reach this table from the root do not already exist, they
+    /// will be created.
+    /// 
+    /// If you give this function a slice consisting of just Key::Char('a')
+    /// and add a binding to the returned table, you can use that binding
+    /// by typing 'a' then that binding.
+    pub fn make_table<T: AsRef<[Key]>>(&mut self, prefix : T) 
+        -> io::Result<usize> 
+    {
+        // The id of the table that does not have the prefix
+        // binding.
+        let mut max_id = 0;
+        let mut max_index = 0;
+        let mut should_make = false;
+
+        // We need to find where we need to start making tables.
+
+        for key in prefix.as_ref().iter() {
+            let mut table = self.get_table_by_id(max_id).unwrap();
+            let binding = table.search_key(*key);
+
+            if binding.is_none() {
+                should_make = true;
+                break;
+            }
+
+            match *binding.unwrap() {
+                Arrow::Table(ref index) => {
+                    max_id = *index;
+                },
+                _ => {
+                    return Err(Error::new(ErrorKind::InvalidInput, "Binding already exists"));
+                }
+            }
+
+            max_index += 1;
+        }
+
+        if should_make {
+            let (_, rest)   = prefix.as_ref().split_at(max_index);
+            let mut last    = max_id;
+            let mut current = self.id_counter;
+
+            for key in rest.iter() {
+                self.new_table();
+            }
+
+            for key in rest.iter() {
+                // jenni is a qt and I am so tired of working on this
+                let mut table = self.get_table_by_id(last).unwrap();
+                let arrow = Arrow::Table(current);
+                table.bind(*key, &arrow);
+
+                current += 1;
+                last     = current;
+            }
+
+            max_id = last;
+        }
+
+        Ok(max_id)
+    }
+
     /// Return to the initial (root) state.
-    pub fn go_home(&mut self) {
+    pub fn to_root(&mut self) {
         self.current_table = 0;
     }
 
